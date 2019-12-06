df0c7a5a17626c4d68ba79c626ed0c8dc5f0de99
Jason Lam <ninewest@gmail.com>
2019-12-06 16:16:34 +0800

    removed redundent user field in Apartment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_06_074939) do
+ActiveRecord::Schema.define(version: 2019_12_06_081444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2019_12_06_074939) do
     t.integer "bedrooms"
     t.boolean "elevator"
     t.boolean "furnished"
-    t.string "user"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
