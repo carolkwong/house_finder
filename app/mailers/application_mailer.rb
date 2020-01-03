871673f5b87686169da4cf6a60ab8a2c8885fec6
Carol Kwong <carol.kwong@gmail.com>
2020-01-03 17:08:11 +0800

    Merged the changes in master into booking_mailer, mainly adj in apartments#show

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'carol.kwong@gmail.com'
   layout 'mailer'
 end
--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -6,8 +6,9 @@ class BookingMailer < ApplicationMailer
   #   en.booking_mailer.confirm.subject
   #
   def confirm
+    @booking = params[:booking]
     @greeting = "Hi"
 
-    mail to: "to@example.org"
+    mail (to: @booking.user.email, subject: "Your booking is confirmed")
   end
 end
