bc18e772a911c2476dd13aca924f7a9d30f1a24c
Carol Kwong <carol.kwong@gmail.com>
2020-01-05 18:15:11 +0800

    Added create booking tab in home page, revised navbar to show current_user emaIL

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -77,6 +77,7 @@ class BookingsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
     def set_booking
       @booking = Booking.find(params[:id])
     end
